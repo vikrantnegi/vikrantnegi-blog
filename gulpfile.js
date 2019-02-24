ddccbf786115cfdcd939c73ea475424925f7e0b9
vikrantnegi <vikrantnegi007@gmail.com>
2019-02-24 17:11:20 +0530

    upgrade gulp and gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -9,9 +9,18 @@ var imagemin = require('gulp-imagemin');
 var cleanCSS = require('gulp-clean-css');
 var rename = require('gulp-rename');
 
-gulp.task('default', function() {
+var paths = {
+  styles: {
+    src: './scss/**/*.scss',
+  },
+  scripts: {
+    src: './js/**/*.js',
+  },
+};
+
+function styles() {
   return gulp
-    .src('./scss/**/*.scss')
+    .src(paths.scripts.src)
     .pipe(sourcemaps.init())
     .pipe(sass({ outputStyle: 'expanded' }).on('error', sass.logError))
     .pipe(
@@ -22,11 +31,14 @@ gulp.task('default', function() {
     )
     .pipe(sourcemaps.write('.'))
     .pipe(gulp.dest('./css'));
-});
+}
 
-/* task to minify the css files
- * SCSS files are converted to CSS and than minified by the following task */
-gulp.task('minify-css', () => {
+/*
+ * task to minify the css files
+ * SCSS files are converted to CSS and than minified by the following task
+ */
+
+function minifyCSS() {
   return gulp
     .src('css/*.css')
     .pipe(
@@ -43,10 +55,10 @@ gulp.task('minify-css', () => {
       }),
     )
     .pipe(gulp.dest('css/min'));
-});
+}
 
 // Optimizing Images
-gulp.task('images', function() {
+function optimiseImages() {
   return (
     gulp
       .src('./img/**/*.+(png|jpg|jpeg|gif|svg)')
@@ -58,10 +70,25 @@ gulp.task('images', function() {
       )
       .pipe(gulp.dest('./img'))
   );
-});
+}
 
-gulp.task('watch', function() {
-  gulp.watch('./scss/**/*.scss', ['default']);
-});
+function watch() {
+  gulp.watch(paths.styles.src, styles);
+}
 
-gulp.task('min', ['watch', 'minify-css']);
+/*
+ * Specify if tasks run in series or parallel using `gulp.series` and `gulp.parallel`
+ */
+var build = gulp.series(gulp.parallel(styles));
+
+/*
+ * You can use CommonJS `exports` module notation to declare tasks
+ */
+
+exports.styles = styles;
+exports.watch = watch;
+exports.build = build;
+/*
+ * Define default task that can be called by just running `gulp` from cli
+ */
+exports.default = build;
